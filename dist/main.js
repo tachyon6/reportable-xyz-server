1b63c0a5d40c06aca63d679db521deac53f88370
tachyon6 <songjunjun62754@gmail.com>
2024-12-28 11:42:17 +0900

    ㄹㄷ

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const swagger_1 = require("@nestjs/swagger");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
+    const logger = new common_1.Logger("App");
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((req, res, next) => {
+        logger.log(`[${req.method}] ${req.url} - IP: ${req.ip}`);
+        next();
+    });
     app.enableCors({
-        origin: ["http://localhost:5173", "http://localhost:3000", "https://reportable.xyz"],
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://reportable.xyz",
+            "https://www.reportable.xyz",
+        ],
         credentials: true,
     });
     const config = new swagger_1.DocumentBuilder()
@@ -18,6 +29,7 @@ async function bootstrap() {
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup("api", app, document);
     await app.listen(8000);
+    logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+    const logger = new Logger("App");
     const app = await NestFactory.create(AppModule);
 
+    // 모든 요청에 대한 로깅 미들웨어
+    app.use((req, res, next) => {
+        logger.log(`[${req.method}] ${req.url} - IP: ${req.ip}`);
+        next();
+    });
+
     app.enableCors({
-        origin: ["http://localhost:5173", "http://localhost:3000", "https://reportable.xyz"],
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "https://reportable.xyz",
+            "https://www.reportable.xyz",
+        ],
         credentials: true,
     });
 
@@ -20,5 +33,6 @@ async function bootstrap() {
     SwaggerModule.setup("api", app, document);
 
     await app.listen(8000);
+    logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
